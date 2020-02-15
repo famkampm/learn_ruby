3a2cc29f62636c84d29d50b11fb6d501f111c345
famkampm <farhanaamiin@gmail.com>
2020-02-15 22:07:16 +0700

    calculator passing bonus case

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -10,3 +10,16 @@ end
 def sum(arr)
   arr.inject(0) { |sum, element| sum += element }
 end
+
+def multiply(*list_of_args)
+  list_of_args.inject(1) { |sum, element| sum *= element }
+end
+
+def pow(number, power_number)
+  number**power_number
+end
+
+def factorial(number)
+  return 1 if number == 0
+  number * factorial(number - 1)
+end
